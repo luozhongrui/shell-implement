924d0b09baf1a58d2d4d700a4309a9fb48300f44
luozhongrui <luozhongrui.cs08@nycu.edu.tw>
2022-10-20 01:29:50 +0800

    parse number pipe

--- src/include/parser.hpp
+++ src/include/parser.hpp
@@ -5,21 +5,28 @@
 
 class Parser {
 private:
-  // std::string command;
   std::set<std::string> bulidInCmd = {"printenv", "setenv", "exit"};
-  // bool ifBuildin;
-  // std::vector<int> numPipSet;
-  // std::vector<std::string> pipeCmdSet; // cmd set from command with pipe
+  std::vector<std::pair<int, std::string>>
+      numPipeCmds; // save pipe cmd and cold down value.
+  std::vector<std::string> launchCmds;
 
 public:
   Parser() {}
   ~Parser() = default;
 
-  bool checkPipExist(std::string &cmd);    // check pip
-  bool checkNumPipExist(std::string &cmd); // check number pip
+  bool checkPipExist(std::string &cmd); // check pip
+  bool checkNumPipExist(std::string &cmd,
+                        std::vector<std::string> &num); // check number pip
   // std::vector<std::string> splitPipCmd();
   std::vector<std::string>
   pipCmdSplit(std::string &cmd); // split cmd form command with pipe
   bool checkBuildin(std::string &cmd);
   void parse(std::string &cmd);
+  void numberPipeCmdDeal(std::string &cmd,
+                         std::vector<std::string> &numPipeSet);
+  void numberCmdCheck(std::string &cmd);
+  void launchPipCmd(bool &flag);
+  std::vector<std::string> findNumPattern(std::string &cmd,
+                                          std::vector<std::string> &num);
+  void oneLineNumDeal(std::vector<std::pair<int, std::string>> &oneline);
 };
--- src/lib/parser.cpp
+++ src/lib/parser.cpp
@@ -11,30 +11,39 @@ bool Parser::checkBuildin(std::string &cmd) {
 bool Parser::checkPipExist(std::string &cmd) {
   return (cmd.find("|") != std::string::npos) ? true : false;
 }
-bool Parser::checkNumPipExist(std::string &cmd) {
-  std::regex pattern("[\\|\\!][\\d]+");
+bool Parser::checkNumPipExist(std::string &cmd, std::vector<std::string> &num) {
+  std::regex pattern("[|!][\\d]+");
   std::string::const_iterator iterStart = cmd.begin();
   std::string::const_iterator iterEnd = cmd.end();
-  std::string temp;
   std::smatch result;
-  if (std::regex_search(iterStart, iterEnd, result, pattern)) {
+  bool flag = false;
+  while (std::regex_search(iterStart, iterEnd, result, pattern)) {
     // std::cout << result[0] << std::endl;
-    return true;
+    num.push_back(result[0]);
+    iterStart = result[0].second;
+    flag = true;
   }
-  return false;
+  return flag;
 }
 
 std::vector<std::string> Parser::pipCmdSplit(std::string &cmd) {
   // split cmd into a vector and get the pip number;
   auto cmdSet = split(cmd, "|");
-  // for (auto cmd : cmdSet) {
-  //   std::cout << cmd << std::endl;
-  // }
   return cmdSet;
 }
 
 void Parser::parse(std::string &cmd) {
   CreateProcess *creater = new CreateProcess();
+  bool flag = false;
+  numberCmdCheck(cmd);
+  launchPipCmd(flag);
+  if (flag)
+    return;
+  std::vector<std::string> numPipeSet;
+  if (checkNumPipExist(cmd, numPipeSet)) {
+    numberPipeCmdDeal(cmd, numPipeSet);
+    return;
+  }
   if (checkBuildin(cmd)) {
     // build_in command
     auto cmdSet = split(cmd, " ");
@@ -48,10 +57,7 @@ void Parser::parse(std::string &cmd) {
       exit(0);
     return;
   }
-  if (checkNumPipExist(cmd)) {
-    // number pipe
-    return;
-  }
+
   if (checkPipExist(cmd)) {
     // origin pipe
     std::vector<std::string> cmdSet = pipCmdSplit(cmd);
@@ -61,4 +67,86 @@ void Parser::parse(std::string &cmd) {
     creater->create(cmd);
   }
   delete creater;
+}
+
+void Parser::numberPipeCmdDeal(
+    std::string &cmd,
+    std::vector<std::string>
+        &numPipeSet) { // there are multi number pipe in middle.
+  auto saveCmds = findNumPattern(cmd, numPipeSet);
+  std::vector<std::pair<int, std::string>> onelineSet;
+  for (size_t i = 0; i < numPipeSet.size(); i++) {
+    std::string num = numPipeSet[i].substr(1);
+    int coldDown = std::stoi(num);
+    onelineSet.push_back(
+        std::make_pair(coldDown, saveCmds[i] + numPipeSet[i][0]));
+  }
+  // update the cmd and cold down time;
+  oneLineNumDeal(onelineSet);
+  numPipeCmds.insert(numPipeCmds.end(), onelineSet.begin(), onelineSet.end());
+  // debug
+  for (auto a : numPipeCmds) {
+    printf("{%d, %s}\n", a.first, a.second.c_str());
+  }
+}
+void Parser::numberCmdCheck(std::string &cmd) {
+  std::vector<int> idx;
+  for (size_t i = 0; i < numPipeCmds.size(); i++) {
+    numPipeCmds[i].first -= 1;
+    std::vector<std::string> numSet1, numSet2;
+    if (numPipeCmds[i].first == 0 && !checkNumPipExist(cmd, numSet1)) {
+      // add cmd into this entry.
+      std::string newcmd = numPipeCmds[i].second + " " + cmd;
+      launchCmds.push_back(newcmd);
+      idx.push_back(i);
+    }
+    if (numPipeCmds[i].first == 0 && checkNumPipExist(cmd, numSet2)) {
+      // add cmd into this entry and update the cold down time.
+      cmd = numPipeCmds[i].second + cmd;
+      numberPipeCmdDeal(cmd, numSet2);
+    }
+  }
+  int j = 0;
+  for (int i : idx) {
+    numPipeCmds.erase(numPipeCmds.begin() + i - j);
+  }
+  idx.clear();
+}
+void Parser::launchPipCmd(bool &flag) {
+
+  // launch cmd and pop the cmd from vector.
+  if (launchCmds.size() > 0)
+    flag = true;
+  for (size_t i = 0; i < launchCmds.size(); i++) {
+    printf("%s\n", launchCmds[i].c_str());
+  }
+  launchCmds.clear();
+}
+
+std::vector<std::string> Parser::findNumPattern(std::string &cmd,
+                                                std::vector<std::string> &num) {
+  std::vector<std::string> pipCmd;
+  int pos = 0;
+  int begin = 0;
+  for (auto patt : num) {
+    pos = cmd.find(patt, pos);
+    pipCmd.push_back(cmd.substr(begin, pos - begin));
+    pos += patt.size();
+    begin = pos;
+  }
+  return pipCmd;
+}
+void Parser::oneLineNumDeal(std::vector<std::pair<int, std::string>> &oneline) {
+  // update the number mid number.
+  int size = oneline.size();
+  std::vector<int> pop;
+  for (int i = 0; i < size; i++) {
+    if (oneline[i].first > size - (i + 1)) {
+      oneline[i].first = oneline[i].first - (size - (i + 1));
+    } else {
+      oneline[i].first = oneline[size - (i + 1)].first;
+      pop.push_back(size - (i + 1));
+      oneline[i].second += " " + oneline[size - (i + 1)].second;
+    }
+  }
 }
